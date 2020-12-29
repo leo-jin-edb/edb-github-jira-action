7f3ca1b37628bb8df6d77a18a7a99b36a9266a93
leo.jin <leo.jin@enterprisedb.com>
2020-12-29 12:36:29 -0500

    axios

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-
+const axios = require('axios');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet')
   const jiraUrl = core.getInput('jira-host')
   console.log(`Hello ${nameToGreet}!  Jira host ${jiraUrl}`)
+  axios.get(jiraUrl).then((res) => {
+      console.log('res = ', res);
+  })
   const time = new Date().toTimeString()
   core.setOutput('time', time)
   // Get the JSON webhook payload for the event that triggered the workflow
