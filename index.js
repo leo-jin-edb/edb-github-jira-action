540a657c0d9103a5719a518fd7689bdecaa7ac12
leo.jin <leo.jin@enterprisedb.com>
2020-12-29 14:08:38 -0500

    jira-129

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ try {
   const jiraUrl = core.getInput('jira-host')
   console.log(`Hello ${nameToGreet}!  Jira host ${jiraUrl}`)
   axios.get(jiraUrl).then((res) => {
-    console.log('res = ', res)
+    // console.log('res = ', res)
   })
   const time = new Date().toTimeString()
   core.setOutput('time', time)
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
   const { commits } = payload;
   const commitMssg = commits[0].message
   if(commitMssg) {
@@ -21,7 +22,6 @@ try {
       const jiraTicket = helper.extractJiraKey(commitMssg);
       console.log('extracted jira tickeet = ', jiraTicket);
   }
-  console.log(`The event payload: ${payload}`)
 } catch (error) {
   core.setFailed(error.message)
 }
