cdcf74eed563c1c9591e2ad3188930caca202796
leo.jin <leo.jin@enterprisedb.com>
2020-12-30 12:52:42 -0500

    tp-5 updated

--- index.js
+++ index.js
@@ -2,37 +2,38 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const axios = require('axios')
 const helper = require('./helper')
-// const jiraApiToken = `0zy6fNhvFPhm0zXFjCxb4685`
+const service = require('./service')
 try {
   // `who-to-greet` input defined in action metadata file
-  const jiraUrl = `${process.env['JIRA_BASE_URL']}/rest/api/latest/project`
-  const jiraApiToken = process.env['JIRA_API_TOKEN']
-  axios.interceptors.request.use(
-    (config) => {
-      console.log('config = ', config)
-      return {
-        ...config,
-        auth: {
-          username: 'leo.jin@enterprisedb.com',
-          password: jiraApiToken,
-        },
-      }
-    },
-    (err) => {
-      Promise.reject(err)
-    }
-  )
+  //   const jiraUrl = `${process.env['JIRA_BASE_URL']}/rest/api/latest/project`
+  //   const jiraApiToken = process.env['JIRA_API_TOKEN']
+  //   axios.interceptors.request.use(
+  //     (config) => {
+  //       console.log('config = ', config)
+  //       return {
+  //         ...config,
+  //         auth: {
+  //           username: 'leo.jin@enterprisedb.com',
+  //           password: jiraApiToken,
+  //         },
+  //       }
+  //     },
+  //     (err) => {
+  //       Promise.reject(err)
+  //     }
+  //   )
 
-  axios
-    .get(jiraUrl)
-    .then((res) => {
-      console.log('res = ', res.data)
-    })
-    .catch((e) => {
-      console.log('error = ', e)
-    })
+  //   axios
+  //     .get(jiraUrl)
+  //     .then((res) => {
+  //       console.log('res = ', res.data)
+  //     })
+  //     .catch((e) => {
+  //       console.log('error = ', e)
+  //     })
   const time = new Date().toTimeString()
   core.setOutput('time', time)
+  core.setOutput('success', true)
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
@@ -43,6 +44,17 @@ try {
     if (commitMssg) {
       console.log('commit message here = ', commitMssg)
       const jiraTicket = helper.extractJiraKey(commitMssg)
+      if (jiraTicket) {
+        // get ticket info
+        service
+          .getJiraTicketDetails(jiraTicket)
+          .then((res) => {
+            console.log('ticket res = ', res.data)
+          })
+          .catch((e) => {
+            console.log('error: ', e)
+          })
+      }
       console.log('extracted jira tickeet = ', jiraTicket)
     }
   }
--- service.js
+++ service.js
@@ -1,3 +1,30 @@
+const { default: axios } = require("axios")
+
+// global vars
+const jiraUrl = `${process.env['JIRA_BASE_URL']}/rest/api/latest`
+const jiraApiToken = process.env['JIRA_API_TOKEN']
+
+// setup auth interceptor
+axios.interceptors.request.use(
+  (config) => {
+    return {
+      ...config,
+      auth: {
+        username: 'leo.jin@enterprisedb.com',
+        password: jiraApiToken,
+      },
+    }
+  },
+  (err) => {
+    Promise.reject(err)
+  }
+)
+
 const getJiraTicketDetails = (ticketId) => {
   console.log(`involking getJiraDetails with ticket id ${ticketId}`)
+  return axios.get(`${jiraUrl}/issue/${ticketId}`);
+}
+
+module.exports = {
+    getJiraTicketDetails
 }
